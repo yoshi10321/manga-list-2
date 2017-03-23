b4a9386b3d4d848d8b83e10aee9dd9105fdfd105
yoshi10321 <pooh.aflac@gmail.com>
2017-03-23 23:12:19 +0900

    del node modules

--- src/jsx/index.jsx
+++ src/jsx/index.jsx
@@ -3,23 +3,25 @@ import { render } from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-import { Router, Route, browserHistory } from 'react-router'
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom'
 import thunk from 'redux-thunk'
 import reducer from './reducers/reducer.jsx'
 import Top from './containers/Top.jsx'
 
-// require('./main.css')
-require('../scss/index.scss')
+// require('../scss/index.scss')
 
 const store = applyMiddleware(thunk)(createStore)(reducer)
 
 render(
-  <MuiThemeProvider>
-    <Provider store={store}>
-      <Router history={browserHistory}>
+  <Router>
+    <MuiThemeProvider>
+      <Provider store={store}>
         <Route path='/' component={Top} />
-      </Router>
-    </Provider>
-  </MuiThemeProvider>,
+      </Provider>
+    </MuiThemeProvider>
+  </Router>,
   document.getElementById('root')
 )
