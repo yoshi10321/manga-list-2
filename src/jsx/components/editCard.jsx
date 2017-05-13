4011822c57af5edde8a9bc50f0f69d77fe1e4e86
yoshi10321 <pooh.aflac@gmail.com>
2017-05-14 05:37:02 +0900

    add style to menu

--- src/jsx/components/editCard.jsx
+++ src/jsx/components/editCard.jsx
@@ -14,9 +14,19 @@ export class EditCard extends React.PureComponent {
     if (editCard.selected) {
       card = (
         <div className='edit-card' key={editCard.id} >
-          <p>{ editCard.title }</p>
-          <p>{ editCard.readNumber }</p>
-          <button onClick={() => dispatch(closeEditCard())}>close</button>
+          <div className='edit-card-content'>
+            <p>{ editCard.title }</p>
+            <p>{ editCard.readNumber }</p>
+          </div>
+          <button className='edit-card-close' onClick={() => dispatch(closeEditCard())}>close</button>
+          <div className='edit-card-menu'>
+            <ul>
+              <li><a>count</a></li>
+              <li><a>title</a></li>
+              <li><a>img</a></li>
+              <li><a>del</a></li>
+            </ul>
+          </div>
         </div>
       )
     }
